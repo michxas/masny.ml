d2efacd59234eb357840395eb333b2233518c560
michxas <100387425+michxas@users.noreply.github.com>
2022-03-25 23:49:24 +0100

    bro idc bro

---
+++ typed.js
@@ -0,0 +1,9 @@
+
+
+new Typed('#typed',{
+  strings : ['masniutki','Black.',';zJKSBD;kwabn','Big Nigger','Black People killer','Rasist.'],
+  typeSpeed : 40,
+  delaySpeed : 40,
+  loop : true
+});
+
